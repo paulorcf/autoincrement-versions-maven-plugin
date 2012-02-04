cf625d2651c4c30265489198a24b1ce45e903b7f
outofcoffee@gmail.com <outofcoffee@gmail.com@20563f5e-d21a-dccb-aee2-282bc4cde9c9>
2012-02-04 02:06:46 +0000

    Adding deploy script and repo

--- repo/deploy-to-repo.sh
+++ repo/deploy-to-repo.sh
@@ -4,7 +4,7 @@
 # to the specified repository location.
 # -------------------------------------
 
-REPO_DEPLOY_PATH="` cd repo && pwd `"
+REPO_DEPLOY_PATH="` pwd `"
 
 echo "Deploying autoincrement-versions-maven-plugin to $REPO_DEPLOY_PATH ..."
 mvn clean deploy -f ../trunk/autoincrement-versions-maven-plugin/pom.xml -Drepo.deploy.path=$REPO_DEPLOY_PATH
